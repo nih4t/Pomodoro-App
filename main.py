f7048e8b33b14d65f8e986fc6e6deeded6d6b098
nih4t <nihatkky@gmail.com>
2024-08-31 23:59:37 +0200

    timer mechanism added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -8,9 +9,40 @@ FONT_NAME = "Courier new"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
+
+# Timer Mechanism
+
+def start_timer():
+    global reps
+    reps +=1
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    if reps % 8 == 0:
+        count_down(long_break_sec)
+        label.config(text="Break", fg=RED)
+    elif reps % 2 == 0:
+        count_down(short_break_sec)
+        label.config(text="Break", fg=PINK)
+    else:
+        count_down(work_sec)
+        label.config(text="Work", fg=GREEN)
 
 
+# Count Down Mechanism
+def count_down(count):
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
 
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
 
 # UI Setup
 
@@ -18,16 +50,18 @@ window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
+
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100, 130, text="00:00", fill= "white", font=(FONT_NAME, 26, "bold"))
+timer_text = canvas.create_text(100, 130, text="00:00", fill= "white", font=(FONT_NAME, 26, "bold"))
 canvas.grid(column=1, row=1)
+
 
 label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50, "bold"))
 label.grid(column=1,row=0)
 
-button_start = Button(text="Start", highlightthickness=0)
+button_start = Button(text="Start", command=start_timer, highlightthickness=0)
 button_start.grid(column=0, row=2)
 
 button_reset = Button(text="Reset", highlightthickness=0)
