cbaf0cbe0ed1ff1c2c2476a585dacd0960a0dcdb
nih4t <nihatkky@gmail.com>
2024-09-01 00:52:04 +0200

    reset and checkmark feature added

--- main.py
+++ main.py
@@ -1,19 +1,38 @@
 from tkinter import *
 import math
+import tkinter.messagebox
 
 PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier new"
-WORK_MIN = 25
+WORK_MIN = 20
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 reps = 0
+timer = None
+marks = ""
+# Timer Reset
+
+def reset_timer():
+    start_button.config(state="normal")
+    reset_button.config(state="disabled")
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text="00:00")
+    label.config(text="Timer", fg=GREEN)
+    check_mark.config(text="")
+    global reps
+    reps = 0
+    global marks
+    marks = ""
+
 
 # Timer Mechanism
 
 def start_timer():
+    start_button.config(state="disabled")
+    reset_button.config(state="normal")
     global reps
     reps +=1
     work_sec = WORK_MIN * 60
@@ -23,16 +42,25 @@ def start_timer():
     if reps % 8 == 0:
         count_down(long_break_sec)
         label.config(text="Break", fg=RED)
+        tkinter.messagebox.showinfo(title="Break", message="Long break!")
+        window.focus_force()
     elif reps % 2 == 0:
+        window.focus_force()
         count_down(short_break_sec)
         label.config(text="Break", fg=PINK)
+        tkinter.messagebox.showinfo(title="Break", message="Short break!")
+        window.focus_force()
+
     else:
         count_down(work_sec)
         label.config(text="Work", fg=GREEN)
+        tkinter.messagebox.showinfo(title="Work", message="Start working~")
+        window.focus_force()
 
 
 # Count Down Mechanism
 def count_down(count):
+    global timer
     count_min = math.floor(count / 60)
     count_sec = count % 60
 
@@ -40,9 +68,15 @@ def count_down(count):
         count_sec = f"0{count_sec}"
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
-        window.after(1000, count_down, count - 1)
+        timer = window.after(1000, count_down, count - 1)
     else:
         start_timer()
+        global marks
+        for _ in range(math.floor(reps/2)):
+            marks += "✔"
+        check_mark.config(text=marks)
+
+
 
 # UI Setup
 
@@ -61,13 +95,13 @@ canvas.grid(column=1, row=1)
 label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50, "bold"))
 label.grid(column=1,row=0)
 
-button_start = Button(text="Start", command=start_timer, highlightthickness=0)
-button_start.grid(column=0, row=2)
+start_button = Button(text="Start", width=8, command=start_timer, state="normal")
+start_button.grid(column=0, row=2)
 
-button_reset = Button(text="Reset", highlightthickness=0)
-button_reset.grid(column=2, row=2)
+reset_button = Button(text="Reset", width=8, command=reset_timer, state="disabled")
+reset_button.grid(column=2, row=2)
 
-check_mark = Label(text="✔", fg=GREEN, bg=YELLOW, highlightthickness=0, font=16)
+check_mark = Label(fg=GREEN, bg=YELLOW, highlightthickness=0, font=16)
 check_mark.grid(column=1, row=3)
 
 window.mainloop()
