88fac79642272c043f4740499055cdbcdac6d726
nih4t <nihatkky@gmail.com>
2024-08-31 23:12:05 +0200

    user interface completed

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
-FONT_NAME = "Courier"
+FONT_NAME = "Courier new"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
@@ -21,7 +21,19 @@ window.config(padx=100, pady=50, bg=YELLOW)
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100, 130, text="00:12", fill= "white", font=(FONT_NAME, 26, "bold"))
-canvas.pack()
+canvas.create_text(100, 130, text="00:00", fill= "white", font=(FONT_NAME, 26, "bold"))
+canvas.grid(column=1, row=1)
+
+label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50, "bold"))
+label.grid(column=1,row=0)
+
+button_start = Button(text="Start", highlightthickness=0)
+button_start.grid(column=0, row=2)
+
+button_reset = Button(text="Reset", highlightthickness=0)
+button_reset.grid(column=2, row=2)
+
+check_mark = Label(text="✔", fg=GREEN, bg=YELLOW, highlightthickness=0, font=16)
+check_mark.grid(column=1, row=3)
 
 window.mainloop()
